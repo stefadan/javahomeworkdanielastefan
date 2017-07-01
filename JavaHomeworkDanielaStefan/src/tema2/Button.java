a3141f1ede833d9dd70ddda60af308935c017ccd
stefadan <iulica@iulian>
2017-07-01 07:13:45 +0300

    Tema3: Set sale price

--- JavaHomeworkDanielaStefan/src/tema2/Button.java
+++ JavaHomeworkDanielaStefan/src/tema2/Button.java
@@ -7,6 +7,16 @@ package tema2;
 
 public class Button {
 
+    
+    
+    /*
+    pentru refacere tema:
+    - ecranul citeste buton si afiseaza buton
+    - trebuie sa ai 13 butoane??
+    
+    */
+    
+    
     int number;
     char operation;
             
